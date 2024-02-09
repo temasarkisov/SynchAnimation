0067d3ef0248eca2703e74d58833efe06117fb72
temasarkisov <a.sarkisov.s@yandex.ru>
2024-02-09 16:33:42 +0200

    Fix code style

--- SynchAnimation/ViewController.swift
+++ SynchAnimation/ViewController.swift
@@ -3,7 +3,10 @@ import UIKit
 final class ViewController: UIViewController {
     private let animatedView = AnimatedView()
     private let slider = UISlider()
-    private let animator = UIViewPropertyAnimator(duration: 1.0, timingParameters: UICubicTimingParameters())
+    private let animator = UIViewPropertyAnimator(
+        duration: 1.0,
+        timingParameters: UICubicTimingParameters()
+    )
  
     private var leadingConstraint: NSLayoutConstraint?
  
@@ -17,7 +20,11 @@ final class ViewController: UIViewController {
  
 extension ViewController {
     private enum LayoutConstants {
-        static let animatedViewSize: CGSize = .init(width: 50, height: 50)
+        static let animatedViewSize: CGSize = .init(
+            width: 50,
+            height: 50
+        )
+        static let animatedViewSizeTopPadding: CGFloat = 20
         static let sliderTopPadding: CGFloat = 15
     }
  
@@ -37,7 +44,10 @@ extension ViewController {
  
         NSLayoutConstraint.activate([
             leadingConstraint,
-            animatedView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
+            animatedView.topAnchor.constraint(
+                equalTo: view.layoutMarginsGuide.topAnchor,
+                constant: LayoutConstants.animatedViewSizeTopPadding
+            ),
             animatedView.widthAnchor.constraint(equalToConstant: LayoutConstants.animatedViewSize.width),
             animatedView.heightAnchor.constraint(equalToConstant: LayoutConstants.animatedViewSize.width)
         ])
@@ -63,7 +73,10 @@ extension ViewController {
         NSLayoutConstraint.activate([
             slider.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
             slider.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            slider.topAnchor.constraint(equalTo: animatedView.bottomAnchor, constant: LayoutConstants.sliderTopPadding)
+            slider.topAnchor.constraint(
+                equalTo: animatedView.bottomAnchor,
+                constant: LayoutConstants.sliderTopPadding
+            )
         ])
     }
 }
@@ -72,12 +85,17 @@ extension ViewController {
     private func setupAnimator() {
         animator.pausesOnCompletion = true
         animator.addAnimations { [weak self] in
-            guard let self else { return }
+            guard let self else {
+                return
+            }
             let xPath = self.calculateAreaPathValue()
             let scaleFactor: CGFloat = 1.5
             self.animatedView.frame.origin.x = xPath
             self.animatedView.transform = CGAffineTransform(rotationAngle: .pi / 2)
-                .scaledBy(x: scaleFactor, y: scaleFactor)
+                .scaledBy(
+                    x: scaleFactor,
+                    y: scaleFactor
+                )
             
             self.view.layoutIfNeeded()
         }
@@ -86,6 +104,7 @@ extension ViewController {
     private func calculateAreaPathValue() -> CGFloat {
         let layoutMarginsHorizontalWidth = view.layoutMargins.right + view.layoutMargins.left
         let animatedViewCurrentWidth = animatedView.bounds.size.width
+        
         return UIScreen.main.bounds.width - layoutMarginsHorizontalWidth - animatedViewCurrentWidth
     }
 }
@@ -97,7 +116,9 @@ extension ViewController {
     }
  
     private func didSliderValueChanged(sender: UISlider?) {
-        guard let value = sender?.value else { return }
+        guard let value = sender?.value else {
+            return
+        }
         animator.fractionComplete = CGFloat(value)
     }
 }
