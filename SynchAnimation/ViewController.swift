2e07be456f3a6574937bbbd0632559c5f59a184c
temasarkisov <a.sarkisov.s@yandex.ru>
2024-02-09 08:48:45 +0200

    Initial Commit

---
+++ SynchAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SynchAnimation
+//
+//  Created by Артем Саркисов on 09.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
